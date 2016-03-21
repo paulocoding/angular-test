64a1f418444e4971276fa4f870b545c32711e5b0
Paulo Silva <engelik@gmail.com>
2016-03-21 12:08:51 +0000

    added dispatch orders feature

--- source/js/2-controllers.js
+++ source/js/2-controllers.js
@@ -1,5 +1,8 @@
 var OrdersListController = function($scope, ordersFactory){
   $scope.orders = ordersFactory.getOrders();
+  $scope.sendOrder = function(order){
+    order.sent = true;
+  };
 };
 OrdersListController.$inject = ["$scope", "ordersFactory"];
 angular.module("ordersApp").controller("OrdersListController",OrdersListController);
