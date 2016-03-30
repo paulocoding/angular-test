4bdbb927b444fabb3a3e1fbda334bce3fc28a5a0
Paulo Silva <engelik@gmail.com>
2016-03-30 10:06:13 +0100

    changed views paths to relative

--- source/js/0-base.js
+++ source/js/0-base.js
@@ -4,42 +4,42 @@ angular.module('ordersApp').config(function($routeProvider){
     .when('/',
     {
       controller: 'OrdersListController',
-      templateUrl: '/app/views/orders.html'
+      templateUrl: 'app/views/orders.html'
     })
     .when('/customer/:customerID',
     {
       controller: 'customerController',
-      templateUrl: '/app/views/customer.html'
+      templateUrl: 'app/views/customer.html'
     })
     .when('/product/:productID',
     {
       controller: 'productController',
-      templateUrl: '/app/views/product.html'
+      templateUrl: 'app/views/product.html'
     })
     .when('/customers/',
     {
       controller: 'customersListController',
-      templateUrl: '/app/views/customers.html'
+      templateUrl: 'app/views/customers.html'
     })
     .when('/products/',
     {
       controller: 'productsListController',
-      templateUrl: '/app/views/products.html'
+      templateUrl: 'app/views/products.html'
     })
     .when('/new-order/',
     {
       controller: 'newOrderController',
-      templateUrl: '/app/views/new-order.html'
+      templateUrl: 'app/views/new-order.html'
     })
     .when('/new-customer/',
     {
       controller: 'newCustomerController',
-      templateUrl: '/app/views/new-customer.html'
+      templateUrl: 'app/views/new-customer.html'
     })
     .when('/new-product/',
     {
       controller: 'newProductController',
-      templateUrl: '/app/views/new-product.html'
+      templateUrl: 'app/views/new-product.html'
     })
     .otherwise(
     {
